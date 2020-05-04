70e83b8821fcb72a07200005b56ed87d346bd430
Kristijan <kristijan.vidovic@five.agency>
2020-05-04 22:57:07 +0200

    Initial init

---
+++ LightSwitch/LightSwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightSwitch
+//
+//  Created by Kristijan Vidovic on 04/05/2020.
+//  Copyright © 2020 Kristijan Vidovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
